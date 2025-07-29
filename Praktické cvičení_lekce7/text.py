331caa483582649a3157a608bc52be70f503b33b
88barcode88 <mira.coufa@gmail.com>
2025-07-29 15:18:34 +0200

    Remove obsolete text.py

--- Praktické cvičení_lekce7/text.py
+++
@@ -1,7 +0,0 @@
-# run_demo.py (nebo v main.py)
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # načte .env do os.environ
-openai_key = os.getenv("OPENAI_API_KEY")
-serpapi_key = os.getenv("SERPAPI_API_KEY")
